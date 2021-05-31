813f6fa5685a3307031f206887ab1c0029ebfa54
Romain Herault <romain.herault99@gmail.com>
2021-05-31 09:04:56 +0200

    fix: doctrine migrations

--- application/src/App/Shared/Infrastructure/Persistence/Doctrine/Migrations/Version20210531065619.php
+++ application/src/App/Shared/Infrastructure/Persistence/Doctrine/Migrations/Version20210531065619.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210531063159 extends AbstractMigration
+final class Version20210531065619 extends AbstractMigration
 {
     public function getDescription(): string
     {
